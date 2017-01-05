393883356f1ee9d25cd52eb56beff65af2a13772
ericamalia <ericamaliafong@gmail.com>
2017-01-05 10:34:54 -0800

    no

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,17 +1,18 @@
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {       
-	String numbers = e.substring(0,10);
+	
+	String numbers = e.substring(2,12);
 	double dubNum = Double.parseDouble(numbers);
-	System.out.println(numbers);  
+	
+		System.out.println(numbers);  
+	
+	
      noLoop();  
 }  
 public void draw()  
 {   
-	int i =0;
-	do{
-		
-	}
+	
 	//not needed for this assignment
 }  
 public boolean isPrime(double dubNum)  
--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -17,18 +17,35 @@ public class GoogleBillboard extends PApplet {
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 {       
-	String numbers = e.substring(0,10);
-	System.out.println(numbers);  
+	
+	String numbers = e.substring(2,12);
+	double dubNum = Double.parseDouble(numbers);
+	
+		System.out.println(numbers);  
+	
+	
      noLoop();  
 }  
 public void draw()  
 {   
+	
 	//not needed for this assignment
 }  
-public boolean isPrime(double dNum)  
+public boolean isPrime(double dubNum)  
 {   
+	if (dubNum < 2){
+		return false;
+	}
+	else{
+		for ( int i =2; i <= Math.sqrt(dubNum); i++){
+			if ( dubNum % i == 0){
+				return true; 
+			}
+		}
+		return false;
+	}
     //to be finished later   
-    return true;  
+  
 } 
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "GoogleBillboard" };
